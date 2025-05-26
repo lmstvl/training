01f06ed6c6871fc59a0f7a642bab3bb8f9b6a436
Mustafa Mestoev <maksmironov142@gmail.com>
2025-05-26 04:54:03 +0300

    Задачка с литкода, процедурным

---
+++ main.py
@@ -0,0 +1,21 @@
+nums = []
+
+while True:
+    user_input = int(input("Введите числа для заполнения списка, если закончили впишите 777777: "))
+    if user_input == 777777:
+        break
+    else:
+        nums.append(user_input)
+target = int(input("Введите цель: "))
+a = 0
+for i in range(len(nums)-1):
+    first_number = nums[i]
+    if a == 1:
+        break
+    for a in range(len(nums)):
+        second_number = nums[a]
+        if first_number != second_number:
+            if first_number + second_number == target:
+                print([nums[i],nums[a]])
+                a = 1
+                break
